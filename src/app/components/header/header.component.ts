b7006da424c76c4079d37b8db021c8fec70daed0
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-09-01 22:12:22 -0300

    exercicio 08

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FavoritesService } from '../../services/favorites.service';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  favoriteCount: number = 0;
 
+  constructor(private favoritesService: FavoritesService) {}
+
+  ngOnInit(): void {
+    this.updateFavoriteCount();
+  }
+
+  updateFavoriteCount(): void {
+    this.favoriteCount = this.favoritesService.getFavorites().length;
+  }
 }
--- src/app/pages/listing/listing.component.ts
+++ src/app/pages/listing/listing.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CarsService } from '../../services/cars.service';
 import { CardComponent } from "../../components/card/card.component";
 import { FavoritesService } from '../../services/favorites.service';
+import { SearchComponent } from "../../components/search/search.component";
 
 @Component({
   selector: 'app-listing',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, SearchComponent],
   templateUrl: './listing.component.html',
   styleUrl: './listing.component.css'
 })
 export class ListingComponent implements OnInit {
   cars: any[] = [];
+  filteredCars: any[] = [];
 
   constructor(
     private carsService: CarsService,
@@ -24,6 +26,13 @@ export class ListingComponent implements OnInit {
         ...car,
         isFavorite: this.favoritesService.isFavorite(car.id)
       }));
+      this.filteredCars = this.cars;
     });
+  }
+
+  onSearch(term: string): void {
+    this.filteredCars = this.cars.filter(car =>
+      car.name.toLowerCase().includes(term.toLowerCase())
+    );
   }
 }
