391df2d8e04080afa29f6e6d82b904283ed21c13
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-09-01 21:58:07 -0300

    exercico 04 e 05

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarsService } from '../../services/cars.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  car: any;
 
+  constructor(private route: ActivatedRoute, private carsService: CarsService) { }
+
+  ngOnInit(): void {
+    const carId = this.route.snapshot.paramMap.get('id');
+    if (carId) {
+      this.carsService.getById(carId).subscribe(data => {
+        this.car = data;
+      });
+    }
+  }
 }
