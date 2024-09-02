dc60ba8daf2a162d80f21705c91586d99df6d448
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-09-01 21:45:46 -0300

    exercicio 03

--- src/app/pages/listing/listing.component.ts
+++ src/app/pages/listing/listing.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarsService } from '../../services/cars.service';
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-listing',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './listing.component.html',
   styleUrl: './listing.component.css'
 })
-export class ListingComponent {
+export class ListingComponent implements OnInit {
+  cars: any[] = [];
 
+  constructor(private carsService: CarsService) { }
+
+  ngOnInit(): void {
+    this.carsService.listAll().subscribe(data => {
+      this.cars = data;
+    });
+  }
 }
