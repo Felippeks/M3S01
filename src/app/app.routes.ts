4ad1c423aa65ca48a0050de86b57a39c391c82ad
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-09-01 21:38:46 -0300

    exercicio 02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListingComponent } from './pages/listing/listing.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/listing', pathMatch: 'full' },  
+  { path: 'listing', component: ListingComponent },
+  { path: 'details/:id', component: DetailsComponent },  
+  { path: '**', redirectTo: '/listing' }
+];
